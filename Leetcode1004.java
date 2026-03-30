ee47dc78c8ec977cd873635595b23eb7d2897857
tranduclong-se <tranduclong.se@gmail.com>
2026-03-30 23:17:46 +0700

    1004

---
+++ Leetcode1004.java
@@ -0,0 +1,26 @@
+//1004. Max Consecutive Ones III
+public class Leetcode1004 {
+    public class Solution {
+        public int longestOnes(int[] nums, int k) {
+            int left = 0;
+            int zeroCount = 0;
+            int maxLength = 0;
+
+            for(int right = 0; right < nums.length;right++){
+                if(nums[right] == 0){
+                    zeroCount++;
+                }
+                while(zeroCount > k){
+                    if(nums[left]==0){
+                        zeroCount--;
+                    }
+                    left++;
+                }
+
+                //Độ dài cửa sổ = right - left + 1
+                maxLength = Math.max(maxLength, right - left + 1);
+            }
+            return maxLength;
+        }
+    }
+}
