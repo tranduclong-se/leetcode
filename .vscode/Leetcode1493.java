429ff1c053f2e703301eef69535fbd9f6cc21c24
tranduclong-se <tranduclong.se@gmail.com>
2026-03-30 23:29:56 +0700

    1493

---
+++ .vscode/Leetcode1493.java
@@ -0,0 +1,24 @@
+//1493. Longest Subarray of 1's After Deleting One 
+public class Leetcode1943 {
+    class Solution {
+        public int longestSubarray (int[] nums) {
+            int left = 0;
+            int rezoCount = 0;
+            int maxLength = 0;
+            for(int right = 0; right < nums.length(); right++){
+                if (nums[right] == 0) zeroCount++;
+
+                while(zeroCount > 1){
+                if(nums[left]==0){
+                    zeroCount--;
+                }
+                left++;
+            }
+            //Tính độ dài: (right - left + 1) là độ dài cửa sổ hiện tại
+            // Trừ đi 1 phần tử bị xóa, ta còn: right - left
+            maxLength = Math.max(maxLength, right - left)
+            }
+            return maxLength;
+        }
+    }
+}
