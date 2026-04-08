7246fca6a34f72727504b98cd5d837625c82fdfa
tranduclong-se <tranduclong.se@gmail.com>
2026-04-09 00:36:14 +0700

    ..

--- Leetcode643.java
+++ Leetcode643.java
@@ -3,14 +3,14 @@ public class Leetcode643 {
     class Solution {
         public double findMaxAverage(int[] nums, int k) {
             double currentSum = 0;
-            for (i =0 ; i<k; i++){
-                currentSum+=nums[i];
+            for (int i = 0; i < k; i++) {
+                currentSum += nums[i];
             }
-            double maxSum = cuum;
+            double maxSum = currentSum;
 
-            for(int i = k ; i< nums.length; i++){
-                //Cộng phần tử mới nums[i] và trừ phần tử cũ nums[i-k].
-                currentSum = currentSum + num[i] - nums[i-k];
+            for (int i = k; i < nums.length; i++) {
+                // Cộng phần tử mới nums[i] và trừ phần tử cũ nums[i-k].
+                currentSum = currentSum + nums[i] - nums[i - k];
                 maxSum = Math.max(maxSum, currentSum);
             }
             return maxSum / k;
